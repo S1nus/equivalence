5a6112cd393f8e1030a3bc30b69784428bf8e830
Connor O'Hara <connor@switchboard.xyz>
2023-12-12 09:44:53 -0500

    small field

--- main.go
+++ main.go
@@ -10,11 +10,22 @@ import (
 
 func main() {
 	modulus := big.NewInt(0).SetUint64(18446744069414584321)
+	var modulusSmall uint64 = 18446744069414584321
 	numBits := numBits(modulus)
+	numBitsSmall := numBitsSmall(modulusSmall)
 	fmt.Println("modulus bitLength: ", numBits)
+	fmt.Println("small modulus bitLength: ", numBitsSmall)
 	share := getRandomShare()
 	fmt.Println(hex.EncodeToString(share[:]))
 }
+
+/*func shareToSmallFields(share [512]byte) uint64 {
+
+}
+
+func shareToBigFields(share [512]byte) []*big.Int {
+
+}*/
 
 func getRandomShare() [512]byte {
 	randomBytes := make([]byte, 512)
@@ -24,6 +35,10 @@ func getRandomShare() [512]byte {
 	}
 	return [512]byte(randomBytes)
 
+}
+
+func numBitsSmall(modulus uint64) int {
+	return int(math.Floor(math.Log2(float64(modulus))))
 }
 
 func numBits(modulus *big.Int) int {
