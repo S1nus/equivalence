72fa34baa15705910df6416122e2dd9760cbf708
Connor O'Hara <connor@switchboard.xyz>
2023-12-12 10:55:00 -0500

    share to smallFields

--- main.go
+++ main.go
@@ -9,7 +9,9 @@ import (
 )
 
 func main() {
+	// SNARKs will use big fields
 	modulus := big.NewInt(0).SetUint64(18446744069414584321)
+	// STARKs and FRI will use more performant uint64s
 	var modulusSmall uint64 = 18446744069414584321
 	numBits := numBits(modulus)
 	numBitsSmall := numBitsSmall(modulusSmall)
@@ -17,13 +19,33 @@ func main() {
 	fmt.Println("small modulus bitLength: ", numBitsSmall)
 	share := getRandomShare()
 	fmt.Println(hex.EncodeToString(share[:]))
+	fmt.Println("converting to small fields:")
+	fields := shareToSmallFields(share, modulusSmall)
+	fmt.Println(fields)
 }
 
-/*func shareToSmallFields(share [512]byte) uint64 {
-
+func shareToSmallFields(share [512]byte, modulus uint64) [8]uint64 {
+	modulusNumBits := numBitsSmall(modulus)
+	if modulusNumBits > 63 {
+		panic("modulusNumBits too large")
+	}
+	shareAsNum := new(big.Int).SetBytes(share[:])
+	mask := new(big.Int).SetInt64(int64(math.Exp2(float64(modulusNumBits))) - 1)
+	fmt.Println("Mask: ", hex.EncodeToString(mask.Bytes()))
+	var results [8]uint64
+	for i := 0; i < 8; i++ {
+		masked := new(big.Int).And(shareAsNum, mask)
+		if masked.BitLen() > modulusNumBits {
+			fmt.Printf("masked bitlen: %v modulusNumBits %v\n", masked.BitLen(), modulusNumBits)
+			panic("too big")
+		}
+		results[i] = masked.Uint64()
+		shareAsNum = shareAsNum.Rsh(shareAsNum, uint(modulusNumBits))
+	}
+	return results
 }
 
-func shareToBigFields(share [512]byte) []*big.Int {
+/*func shareToBigFields(share [512]byte) []*big.Int {
 
 }*/
 
